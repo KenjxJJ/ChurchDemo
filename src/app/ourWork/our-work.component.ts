b1f8906fd73f31b77d2b86d8223e1b797a738268
JOEL KIGGWE <joelkiggwe94@gmail.com>
2020-05-28 01:02:44 +0300

    Edit ourWork component

--- src/app/ourWork/our-work.component.ts
+++ src/app/ourWork/our-work.component.ts
@@ -4,7 +4,9 @@ import { Component } from "@angular/core";
    selector: 'our-work',
    templateUrl: './our-work.component.html',
    styles :[
-         
+          ` main  {
+              padding: 1.4rem;
+          }`
    ]
 })
 
